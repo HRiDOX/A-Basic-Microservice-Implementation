28c20bc310e73dd9145415cfc2314b6a4dbdf2c7
HRiDOX <rgrrf123@gmail.com>
2023-01-22 20:34:34 +0600

    using query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,8 +1,7 @@
 import React,{useState, useEffect} from 'react';
-import axios from 'axios';
 
-const CommentList = ({postId}) => {
-    const [comments, setComments] = useState([]);
+const CommentList = ({ comments}) => {
+   /* const [comments, setComments] = useState([]);
 
     const fetchData = async () => {
         const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
@@ -14,7 +13,7 @@ const CommentList = ({postId}) => {
     useEffect(() => {
         fetchData();
     },[]);
-
+*/
     const renderedComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
     });
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,8 +7,13 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        // previously we use this 4000 port to get post directly from post service
+        // now we used to emit posts when some one created post thn query service catch these events[all other post and comments service also catch events]
+        //const res = await axios.get('http://localhost:4000/posts');
+        // now we will use 4002 port to get post data from query service instead of getting data directly from a particular post service
+        const res = await axios.get('http://localhost:4002/posts');
 
+        
         setPosts(res.data);
     };
     //first ei jate post fetch kora hpy that's why useEffect
@@ -27,7 +32,7 @@ const PostList = () => {
         >
             <div className="card-body">
                 <h3>{post.title}</h3>
-                <CommentList postId={post.id} />
+                <CommentList comments={post.comments} />
                 <CommentCreate postId={post.id}/>
             </div>
 
